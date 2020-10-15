db822202d92b05471b51ce1d3a64cc048c3b8dc3
Beksultan <beksultankaiypov@gmail.com>
2020-10-15 18:51:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
    
-   planeteer_calls.include?(valid_calls)
-   valid_calls
+      planeteer_calls.find { |words| planeteer_calls.include?(words) }
 
 end
 
